26770d0ad1e876436ff30a26e99841fe7cf05754
jins5 <karim.oubram@etu.umontpellier.fr>
2024-07-26 23:57:33 +0200

    push lben

---
+++ src/app/lben/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Ma page Next.js</h1>
+            <p>Ceci est une page simple créée avec Next.js.</p>
+        </div>
+    );
+};
+
+export default Page;
