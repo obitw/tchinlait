25daab60005f3ba949e7eb80c5aa45278919d9ef
micro <ilias.ouchman@etu.umontpellier.fr>
2024-08-14 14:53:20 +0200

    ajout pages navbar

---
+++ src/app/nutrition/page.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div>
+			<h1>Conseils et nutrition</h1>
+			<p>Saviez vous que:</p>
+			<ul>
+				<li>L’être humain construit l’essentiel de son capital calcique (masse minérale osseuse) jusqu&apos;à l’âge de 20 ans.</li>
+				<li>À partir de 50 ans, cette masse osseuse diminue de 1 à 2% annuellement.</li>
+				<li>Voila pourquoi l’apport en calcium dès le jeune âge, est primordial pour prévenir tous risques liés à une décalcification des os.</li>
+				<li>Le calcium à un rôle essentiel dans la constitution des os.</li>
+				<li>100ml de lait Candia contiennent 110mg de calcium.</li>
+				<li>Un bol de lait Candia apporte plus de 300mg de calcium (entre 25 et 30% de nos besoins quotidiens).</li>
+				<li>1 litre de lait Candia = 30 g de protéines.</li>
+				<li>Les apports journaliers recommandés en calcium sont de 900mg chez l’adulte et peuvent monter jusqu&apos;à 1200mg pour les adolescents, les femmes enceintes et celles qui allaitent.</li>
+				<li>Le magnésium intervient dans le fonctionnement musculaire, dans les phénomènes de contraction et de relâchement.</li>
+			</ul>
+			
+			<p>Les laits Candia Contiennent:</p>
+			<ul>
+				<li>La vitamine B9 est nécessaire à la formation des globules rouges, la prévention des maladies cardiovasculaires et au développement neurologique du fœtus chez la femme enceinte.</li>
+				<li>Les vitamines B1, B2, B3 et B6 jouent un rôle dans le fonctionnement des systèmes nerveux et musculaires.</li>
+				<li>La vitamine B5 favorise la croissance et la résistance de la peau.</li>
+				<li>La vitamine B12 joue un rôle important dans la mémorisation.</li>
+				<li>Un Lait riche en vitamines B2 et B12.</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Page;
