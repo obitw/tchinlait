070026372da02b16a8080eb3afcb2a6f16dae430
ilias <ilias.ouchman@etu.umontpellier.fr>
2025-08-24 13:41:31 +0100

    clean images

--- src/data/portfolio_data.ts
+++ src/data/portfolio_data.ts
@@ -4,8 +4,7 @@ import portfolio_img_3 from "@/assets/img/gallery/feature3.jpg";
 import portfolio_img_4 from "@/assets/img/gallery/feature4.jpg";
 import portfolio_img_5 from "@/assets/img/gallery/feature5.jpg";
 import portfolio_img_6 from "@/assets/img/gallery/feature6.jpg";
-import portfolio_img_7 from "@/assets/img/gallery/feature7.jpg";
-import portfolio_img_8 from "@/assets/img/gallery/feature8.jpg";
+
 import { StaticImageData } from "next/image";
 
 interface PortfolioDataType {
@@ -64,56 +63,7 @@ const portfolio_data: PortfolioDataType[] = [
     category: "Produits laitiers",
     description:
       "Un moment de pure douceur avec chaque gorgée de notre nectar de grenade.️",
-  },
-  // data repeat
-  {
-    id: 1,
-    title: "Lait frais",
-    image: portfolio_img_7,
-    category: "Produits laitiers",
-    description:
-      "Lait frais de qualité, directement issu de nos fermes partenaires.",
-  },
-  {
-    id: 2,
-    title: "Yaourt nature",
-    image: portfolio_img_8,
-    category: "Produits laitiers",
-    description:
-      "Yaourts naturels, riches en probiotiques pour une digestion saine.",
-  },
-  {
-    id: 3,
-    title: "Fromage crémeux",
-    image: portfolio_img_3,
-    category: "Produits laitiers",
-    description:
-      "Fromage crémeux fabriqué selon des méthodes traditionnelles et naturelles.",
-  },
-  {
-    id: 4,
-    title: "Beurre",
-    image: portfolio_img_4,
-    category: "Produits laitiers",
-    description:
-      "Beurre frais, préparé à partir du lait de vaches nourries avec des aliments naturels.",
-  },
-  {
-    id: 5,
-    title: "Crème fraîche",
-    image: portfolio_img_5,
-    category: "Produits laitiers",
-    description:
-      "Crème fraîche d'excellence pour enrichir vos recettes culinaires.",
-  },
-  {
-    id: 6,
-    title: "Lait de vache",
-    image: portfolio_img_6,
-    category: "Produits laitiers",
-    description:
-      "Lait de vache pasteurisé, garantissant fraîcheur et qualité pour toute la famille.",
-  },
+  }
 ];
 
 export default portfolio_data;
